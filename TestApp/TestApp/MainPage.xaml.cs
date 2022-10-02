278cfcc0c1da3b38a607aae53b01ec586178e538
Jonas Marvin Huhndorf <jonas.huhndorf@gmail.com>
2022-10-02 16:29:16 +0200

    Add application

---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace TestApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnBtnClicked(object sender, EventArgs e) {
+        FilePickerFileType gpxFileType = new(
+                new Dictionary<DevicePlatform, IEnumerable<string>> {
+                    {DevicePlatform.Android, new[] { "application/gpx+xml", "application/gpx", "text/xml" } },
+                    {DevicePlatform.WinUI, new[] { ".gpx" } },
+                }
+                );
+
+        PickOptions options = new() {
+            PickerTitle = "Select a gpx file",
+            FileTypes = gpxFileType,
+        };
+
+        FileResult result = await FilePicker.PickAsync(options);
+
+        showResult.Text = result.FileName;
+    }
+}
+
